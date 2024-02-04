24dca0889f43752e62a14c6416bf5770e3822904
ArsiOsmovi <arsi.osmovi@gmail.com>
2024-02-04 11:30:55 +0100

    Update

---
+++ src/main/java/Converter/ProductConverter.java
@@ -0,0 +1,11 @@
+package Converter;
+
+import Entity.Product;
+
+public class ProductConverter {
+
+//    public static ProductEntity convertToEntity(ProductModel){
+//        Product product = new Product();
+//        product.setName(model.getName());
+//    }
+}
